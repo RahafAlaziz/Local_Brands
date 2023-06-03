31d1cac7c18a304f1f84ee3681b417cad837bc64
Lujain <lujain_mohammed@outlook.sa>
2023-06-03 14:56:38 +0300

    Initial Commit

---
+++ Local_Brands/Local_BrandsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Local_BrandsApp.swift
+//  Local_Brands
+//
+//  Created by lujain mohammed on 14/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct Local_BrandsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
